5b411df32ec60bd07102b9231b49ecbeeab9150d
Nikita <isfellop@gmail.com>
2020-04-27 18:53:26 +0300

    +1 service (wow)

---
+++ b0mb3r/services/nncard.py
@@ -0,0 +1,11 @@
+from service import Service
+
+
+class NNCard(Service):
+    phone_codes = ["7"]
+
+    async def run(self):
+        await self.post(
+            "https://nn-card.ru/api/1.0/covid/login",
+            json={"phone": self.formatted_phone,},
+        )
