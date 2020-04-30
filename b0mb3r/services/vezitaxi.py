d6a91b63c5f2f6555de7e391750397f9d33608ba
crinny <isfellop@gmail.com>
2020-04-30 22:43:47 +0300

    +2 services :sparkles:

---
+++ b0mb3r/services/vezitaxi.py
@@ -0,0 +1,13 @@
+from service import Service
+
+
+class VeziTaxi(Service):
+    async def run(self):
+        await self.get(
+            "https://vezitaxi.com/api/employment/getsmscode",
+            params={
+                "phone": "+" + self.formatted_phone,
+                "city": 561,
+                "callback": "jsonp_callback_35979",
+            },
+        )
