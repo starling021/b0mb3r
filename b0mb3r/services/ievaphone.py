1550bb05cd1ccacadea651ae0ae58a0449ff1897
crinny <crinny@users.noreply.github.com>
2020-06-22 14:13:38 +0300

    :sparkles: +3 services

---
+++ b0mb3r/services/ievaphone.py
@@ -0,0 +1,9 @@
+from b0mb3r.services.service import Service
+
+
+class IevaPhone(Service):
+    async def run(self):
+        await self.get(
+            "https://ievaphone.com/call-my-phone/web/request-free-call",
+            params={"phone": self.formatted_phone, "domain": "IEVAPHONE", "browser": "undefined"},
+        )
